329c370548c7569d1b205c2acf9ae78a99ee13da
sanjayav <svithanagama@gmail.com>
2025-03-02 12:18:55 +1100

    cnsia: Order service initial commit.

---
+++ src/main/java/me/sanjayav/polarbookshop/orderservice/CnsiaOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package me.sanjayav.polarbookshop.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsiaOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsiaOrderServiceApplication.class, args);
+	}
+
+}
