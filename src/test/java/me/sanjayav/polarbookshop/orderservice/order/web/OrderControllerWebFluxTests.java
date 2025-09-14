d767b640e67900c5b2365181164019ae81a880eb
sanjayav <svithanagama@gmail.com>
2025-09-14 18:22:24 +1000

    cnsia: Security configuration for order-service.

--- src/test/java/me/sanjayav/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
+++ src/test/java/me/sanjayav/polarbookshop/orderservice/order/web/OrderControllerWebFluxTests.java
@@ -1,10 +1,15 @@
 package me.sanjayav.polarbookshop.orderservice.order.web;
 
 
+import me.sanjayav.polarbookshop.orderservice.config.SecurityConfig;
 import me.sanjayav.polarbookshop.orderservice.order.domain.Order;
 import me.sanjayav.polarbookshop.orderservice.order.domain.OrderService;
 import me.sanjayav.polarbookshop.orderservice.order.domain.OrderStatus;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import reactor.core.publisher.Mono;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @WebFluxTest(OrderController.class)
+@Import(SecurityConfig.class)
 class OrderControllerWebFluxTests {
 
   @Autowired
@@ -21,6 +27,9 @@ class OrderControllerWebFluxTests {
 
   @MockitoBean
   private OrderService orderService;
+
+  @MockitoBean
+  private ReactiveJwtDecoder reactiveJwtDecoder;
 
   @Test
   void whenBookNotAvailableThenRejectOrder() {
@@ -32,6 +41,8 @@ class OrderControllerWebFluxTests {
     ).willReturn(Mono.just(expectedOrder));
 
     webClient
+        .mutateWith(SecurityMockServerConfigurers
+        .mockJwt().authorities(new SimpleGrantedAuthority("ROLE_customer")))
         .post()
         .uri("/orders")
         .bodyValue(orderRequest)
